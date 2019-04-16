560fb0eb24e00e84a901c4dbe300fbdb755d9d3e
Kenny Halim <kennyhalim98@gmail.com>
2019-04-16 14:17:50 -0700

    fnish login and signup page

---
+++ app/app.js
@@ -0,0 +1,16 @@
+import Vue from "nativescript-vue";
+
+import Home from "./components/Home";
+
+
+new Vue({
+
+    template: `
+        <Frame>
+            <Home />
+        </Frame>`,
+
+    components: {
+        Home
+    }
+}).$start();
